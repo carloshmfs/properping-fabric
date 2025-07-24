9411d67655771b79a4b55bb2235cb40acb4765f3
Carlos Santos <carloshmfs.dev@pm.me>
2025-07-24 17:02:48 -0300

    initial commit

---
+++ src/client/java/dev/carloshmfs/ProperPingfabricClient.java
@@ -0,0 +1,10 @@
+package dev.carloshmfs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProperPingfabricClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
