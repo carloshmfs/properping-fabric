0c266448afe6aa0d95d3c146c5ccdf11b5d276c1
Carlos Santos <carloshmfs.dev@pm.me>
2025-08-14 02:23:35 -0300

    feat: latency info cache

---
+++ src/main/java/dev/carloshmfs/LatencyInfo.java
@@ -0,0 +1,4 @@
+package dev.carloshmfs;
+
+public class LatencyInfo {
+}
--- src/main/java/dev/carloshmfs/networking/PingPongHandler.java
+++ src/main/java/dev/carloshmfs/networking/PingPongHandler.java
@@ -1,5 +1,8 @@
 package dev.carloshmfs.networking;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import dev.carloshmfs.LatencyInfo;
 import dev.carloshmfs.ProperPingfabric;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
@@ -7,11 +10,19 @@ import net.minecraft.Util;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
 
+import java.time.Duration;
+import java.util.UUID;
+
 public class PingPongHandler {
+    private static final Cache<UUID, LatencyInfo> latencyInfoCache = CacheBuilder.newBuilder().expireAfterWrite(Duration.ofSeconds(35)).build();
+
     public static void handlePing(final ServerPlayer player) {
         if (player == null) {
             return;
         }
+
+        UUID playerUUID = player.getUUID();
+        LatencyInfo latencyInfo = getPlayerLatencyInfo(playerUUID);
 
         long currentTime = Util.getMillis();
         if (currentTime % 5 == 0) {
@@ -23,7 +34,13 @@ public class PingPongHandler {
 
     public static void handlePong(final ServerPlayer player, long originalTime) {
         long latency = Util.getMillis() - originalTime;
+        UUID playerUUID = player.getUUID();
+        LatencyInfo latencyInfo = getPlayerLatencyInfo(playerUUID);
 
-        ProperPingfabric.LOGGER.info("PING: " + latency);
+    }
+
+    private static LatencyInfo getPlayerLatencyInfo(UUID playerUUID) {
+        LatencyInfo lantencyInfo = latencyInfoCache.getIfPresent(playerUUID);
+        return lantencyInfo != null ? lantencyInfo : new LatencyInfo();
     }
 }
