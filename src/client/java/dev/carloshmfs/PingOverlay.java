886e503e552250bb8cdad8c61e9bf6d2c19dc693
Carlos Santos <carloshmfs.dev@pm.me>
2025-08-16 18:24:40 -0300

    feat: increase tablist ping column width

--- src/client/java/dev/carloshmfs/PingOverlay.java
+++ src/client/java/dev/carloshmfs/PingOverlay.java
@@ -8,11 +8,13 @@ import net.minecraft.network.chat.Component;
 public class PingOverlay {
     public static final PingOverlay INSTANCE = new PingOverlay();
     public int averageLatency = 0;
+    public final int pingColumnWidth;
 
     private final Minecraft minecraft;
 
     PingOverlay() {
         this.minecraft = Minecraft.getInstance();
+        this.pingColumnWidth = this.minecraft.font.width(Component.translatable("multiplayer.status.ping", 999));
     }
 
     public static PingOverlay getInstance() {
@@ -26,7 +28,7 @@ public class PingOverlay {
 
         int latency = this.minecraft.player.getGameProfile().getName().equalsIgnoreCase(playerInfo.getProfile().getName()) ? this.averageLatency : playerInfo.getLatency();
         Component component = Component.translatable("multiplayer.status.ping", latency);
-        guiGraphics.drawString(this.minecraft.font, component, x + columnWidth , y, 0x00FF00);
+        guiGraphics.drawString(this.minecraft.font, component, x + columnWidth - this.minecraft.font.width(component), y, 0x00FF00);
 
         return true;
     }
--- src/client/java/dev/carloshmfs/mixin/client/PlayerTabOverlayMixin.java
+++ src/client/java/dev/carloshmfs/mixin/client/PlayerTabOverlayMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.client.multiplayer.PlayerInfo;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerTabOverlay.class)
@@ -16,5 +17,10 @@ public class PlayerTabOverlayMixin {
 		if (PingOverlay.getInstance().render(guiGraphics, columnWidth, x, y, playerInfo)) {
 			info.cancel();
 		}
+	}
+
+	@ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Ljava/lang/Math;min(II)I"), index = 0)
+	private int overwriteTablistColumnWidth(int vanillaWidth) {
+		return vanillaWidth + PingOverlay.getInstance().pingColumnWidth;
 	}
 }
